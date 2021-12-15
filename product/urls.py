41809f98338ff89719f470075914666d101cffcc
Ekasit Ja <ekasit.ja@gmail.com>
2021-12-15 18:01:33 +0700

    finish draft of product page

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 app_name = 'product'
 urlpatterns = [
-    path('milg', product_milg, name='product-milg'),
+    path('wind', product_wind, name='product-wind'),
+    path('move', product_move, name='product-move'),
+    path('max', product_max, name='product-max'),
+    path('flash', product_flash, name='product-flash'),
 ]
--- product/views.py
+++ product/views.py
@@ -4,8 +4,35 @@ from django.template.defaultfilters import striptags, truncatechars
 from django.templatetags.static import static
 
 # Create your views here.
-def product_milg(request):
-    return render(request, 'product/milg.html' , {
+def product_wind(request):
+    return render(request, 'product/wind.html' , {
+        'meta_title': _('pcj-industries-long'),
+        'meta_desc': _('meta-desc-home'),
+        'meta_kw': _('meta-kw-home'),
+        'meta_img': request.build_absolute_uri(static('images/pcj-logo-og.jpg')),
+        'meta_robots': 'index, follow',
+    })
+
+def product_move(request):
+    return render(request, 'product/move.html' , {
+        'meta_title': _('pcj-industries-long'),
+        'meta_desc': _('meta-desc-home'),
+        'meta_kw': _('meta-kw-home'),
+        'meta_img': request.build_absolute_uri(static('images/pcj-logo-og.jpg')),
+        'meta_robots': 'index, follow',
+    })
+
+def product_max(request):
+    return render(request, 'product/max.html' , {
+        'meta_title': _('pcj-industries-long'),
+        'meta_desc': _('meta-desc-home'),
+        'meta_kw': _('meta-kw-home'),
+        'meta_img': request.build_absolute_uri(static('images/pcj-logo-og.jpg')),
+        'meta_robots': 'index, follow',
+    })
+
+def product_flash(request):
+    return render(request, 'product/flash.html' , {
         'meta_title': _('pcj-industries-long'),
         'meta_desc': _('meta-desc-home'),
         'meta_kw': _('meta-kw-home'),
